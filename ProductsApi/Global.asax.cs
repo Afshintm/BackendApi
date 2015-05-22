fa7042ce54583de2fa806f496f7df034514acce9
Afshin Teymoori <afshin_tm@yahoo.com>
2015-05-22 11:07:36 +1000

    add Ioc to the project

--- ProductsApi/Global.asax.cs
+++ ProductsApi/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using Autofac.Integration.WebApi;
+using Ioc.Core.Infrastructure;
 
 namespace ProductsApi
 {
@@ -11,7 +13,22 @@ namespace ProductsApi
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+			// initialize engine context using the Ninja.Core
+			// Running the Build method on the container
+			// finding instances of IDependencyRegistrar and running its register method 
+
+			EngineContext.Initialize(false);
+
+			//set dependency resolver for webApi
+			GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(EngineContext.Current.ContainerManager.Container);
+
+			// set DependencyResolver for MVC 
+			//DependencyResolver.SetResolver(new AutofacDependencyResolver(EngineContext.Current.ContainerManager.Container);
+
+
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+
         }
     }
 }
--- ProductsApi/Models/Product.cs
+++ ProductsApi/Models/Product.cs
@@ -1,10 +1,10 @@
 ﻿
 namespace ProductsApi.Models
 {
-	public class Product
+	public class Product:EntityBase
 	{
-		public int Id { get; set; }
-		public string Name { get; set; }
+		//public int Id { get; set; }
+		//public string Name { get; set; }
 		public string Category { get; set; }
 		public decimal Price { get; set; }
 	}
