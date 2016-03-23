d464d81275f42459369472883de1a0c5073be111
Afshin Teymoori <afshin_tm@yahoo.com>
2016-03-23 12:35:37 +1100

    Add 3 projects for Oauth2 identity Server

---
+++ IdentityServer/IdentityServer.Host.Config/FactoryExtensions.cs
@@ -0,0 +1,29 @@
+﻿
+
+using IdentityServer.Host.Configuration.Extensions;
+using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Services;
+
+namespace IdentityServer.Host.Configuration
+{
+    static class FactoryExtensions
+    {
+        public static IdentityServerServiceFactory AddCustomGrantValidators(this IdentityServerServiceFactory factory)
+        {
+            factory.CustomGrantValidators.Add(
+                    new Registration<ICustomGrantValidator, CustomGrantValidator>());
+            factory.CustomGrantValidators.Add(
+                new Registration<ICustomGrantValidator, AnotherCustomGrantValidator>());
+
+            return factory;
+        }
+
+        public static IdentityServerServiceFactory AddCustomTokenResponseGenerator(this IdentityServerServiceFactory factory)
+        {
+            factory.CustomTokenResponseGenerator = 
+                new Registration<ICustomTokenResponseGenerator, CustomTokenResponseGenerator>();
+
+            return factory;
+        }
+    }
+}
---
+++ IdentityServer/IdentityServer.Host.Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Owin;
+using Serilog;
+
+namespace IdentityServer.Host.Web
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.WriteTo.Trace()
+				.CreateLogger();
+
+			app.UseIdentityServer();
+		}
+	}
+}
