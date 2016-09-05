851d0247a5c62637d74c1bd5b1e12ec5622e3c0e
Afshin Teymoori <afshin_tm@yahoo.com>
2016-09-05 16:56:14 +1000

    returning claims json from Test controller and setup the solution to run IdentityServer.Host.Web as Identity server, ProductsApi as Api which is using Identity Server to authenticate the requests and JavaScript Client Manual which is a simple Html based calling webapi with authentication through identity server

--- ProductsApi/Controllers/TestController.cs
+++ ProductsApi/Controllers/TestController.cs
@@ -1,16 +1,18 @@
-﻿using ProductsApi.Infrastructure;
+﻿using Newtonsoft.Json;
+using ProductsApi.Infrastructure;
 using SalesModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
 namespace ProductsApi.Controllers
 {
-    [RoutePrefix("api/Tests")]
+    [RoutePrefix("test")]
     [EnableCors("*", "*", "*")]
     public class TestController : BaseApiController
     {
@@ -18,12 +20,48 @@ namespace ProductsApi.Controllers
         public TestController(Func<string, IClass1> class1Facory) {
             _class1Factory = class1Facory;
         }
-        [Route("")]
+        [Route("name")]
         [HttpGet]
-        public IHttpActionResult GetAllTest() {
+        public IHttpActionResult GetName() {
             IClass1 c1 = _class1Factory("afshin");
             
             return Ok( c1.Name);
         }
+        [Route("")]
+        [HttpGet]
+        public IHttpActionResult GetAlls()
+        {
+            //var caller = User as ClaimsPrincipal;
+            var claimsPrincipal = User as ClaimsPrincipal;
+            var returnJson = String.Empty;
+
+            //var subjectClaim = caller.FindFirst("sub");
+            //if (subjectClaim != null)
+            //{
+            //    return Json(new
+            //    {
+            //        message = "OK user",
+            //        client = caller.FindFirst("client_id").Value,
+            //        subject = subjectClaim.Value
+            //    });
+            //}
+            //else
+            //{
+            //    return Json(new
+            //    {
+            //        message = "OK computer",
+            //        client = caller.FindFirst("client_id").Value
+            //    });
+            //}
+            //simply returning jsonObject claimsPrincipal if there is any
+            if (claimsPrincipal != null)
+                returnJson = JsonConvert.SerializeObject(claimsPrincipal, Formatting.Indented,
+                    new JsonSerializerSettings
+                    {
+                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                    });
+            return Ok(returnJson);
+        }
+
     }
 }
